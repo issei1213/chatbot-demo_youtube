9722c6f2e53c381b40f4f8fcd145c9d7a7eebf3f
lord_0bf38w16110335h@yahoo.co.jp <59830008+issei1213@users.noreply.github.com>
2020-09-27 15:02:25 +0900

    completed fix #10

---
+++ chatbot-demo/src/firebase/config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDA699hZAIbfDK-hKUMfXeLzoO6uV8Ypq8",
+  authDomain: "chatbot-demo-dd828.firebaseapp.com",
+  databaseURL: "https://chatbot-demo-dd828.firebaseio.com",
+  projectId: "chatbot-demo-dd828",
+  storageBucket: "chatbot-demo-dd828.appspot.com",
+  messagingSenderId: "647068696846",
+  appId: "1:647068696846:web:f52e5bbd9a069d86b3d75f",
+  measurementId: "G-ZP4SF8X8JX"
+};
+
+export default firebaseConfig
